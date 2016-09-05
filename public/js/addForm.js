47bb9e6024854df4d5a662b23cea992280a825f9
Sebastián Perez <seperezf@gmail.com>
2016-09-04 23:03:06 -0300

    Nuevo estilo formulario de presupestos

--- public/js/addForm.js
+++ public/js/addForm.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-    $("input[name='email']").focusout(function(){
-
-        var url = "/api/getcliente/" + $(this).val();
+    $("a[name='veriflink']").click(function(event){
+        event.preventDefault();
+        var url = "/api/getcliente/" + $("input[name='email']").val();
 
         $.getJSON(url, function(data) {
             if(data.resultado == 'exito')
@@ -10,19 +10,12 @@ $(document).ready(function(){
                 $("div[name='ntfExito']").removeClass('hidden');
                 $("div[name='ntfNuevo']").addClass('hidden');
                 $("input[name='nombre']").val(data.cliente.nombre);
-                $("input[name='nombre']").attr('disabled','true');
-                $("input[name='cliente_id']").val(data.cliente.id);
             }else {
                 $("div[name='ntfNuevo']").removeClass('hidden');
                 $("div[name='ntfExito']").addClass('hidden');
                 $("input[name='nombre']").val('');
-                $("input[name='nombre']").removeAttr('disabled');
-                $("input[name='cliente_id']").val(0);
             }
-
-            $("textarea[name='comentario']").removeAttr('disabled');
-            $("button[name='enviar']").removeAttr('disabled');
         });
-    });
 
+    });
 })
--- resources/views/admin/addForm.blade.php
+++ resources/views/admin/addForm.blade.php
@@ -4,6 +4,7 @@
 @section('head')
 
 <script type="text/javascript" src="/js/addForm.js"></script>
+<link rel="stylesheet" href="/css/estilos.css">
 
 @endsection
 
@@ -15,32 +16,34 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <h3>Presupuesto nuevo</h3>
+                <h3 class="tituloAddForm">Presupuesto nuevo</h3>
                 <form method="POST" action="/admin/presupuesto">
                     <div class="form-group">
                         <label for="email">Correo electrónico:</label>
-                        
-                        <input type="email" name="email" class="form-control"
-                         placeholder="correo@example.com" required >
+                        <div class="input-group">
+                            <input type="email" name="email" class="form-control" placeholder="correo@example.com"
+                            value="{{old('email')}}" required >
+                            <div class="input-group-addon"><a href="#" name="veriflink">Verificar</a></div>
+                        </div>
                     </div>
                     <div name ='ntfExito' class='alert alert-success hidden' role='alert'>Cliente existente</div>
                     <div name ='ntfNuevo' class='alert alert-info hidden' role='alert'>Cliente Nuevo</div>
                     <div class="form-group">
                         <label for="nombre">Nombre:</label>
-                        <input type="text" name="nombre" class="form-control" disabled>
+                        <input type="text" name="nombre" class="form-control" value="{{old('nombre')}}" required>
                     </div>
 
                     <div class="form-group">
                         <label for="comentario">Comentario:</label>
-                        <textarea name="comentario" class="form-control" disabled></textarea>
+                        <textarea name="comentario" class="form-control">{{old('comentario')}}</textarea>
                     </div>
-                    <input type="hidden" name="cliente_id" value="0">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <button name="enviar" type="submit" class="btn btn-default" disabled>Guardar</button>
-                    </div>
+                        <button name="enviar" type="submit" class="btn btn-default">Guardar</button>
+                    </div>  
                 </form>
                 @include('components.flash')
+                @include('components.errors')
             </div>
         </div>
     </div>
