f161cca47929e13953f6c1b2a2cf2238ce8c8b72
Sebastián Perez <seperezf@gmail.com>
2016-12-13 23:15:35 +0000

    Arreglo el seeder de Presupuestos

--- database/seeds/PresupuestosSeeder.php
+++ database/seeds/PresupuestosSeeder.php
@@ -15,8 +15,8 @@ class PresupuestosSeeder extends Seeder
             $u->precios()->save(factory(App\Precio::class)->make());
         });
 
-        factory(App\Presupuesto::class)->states('noUser')->create()->each(function ($u) {
-            $u->precios()->save(factory(App\Precio::class)->make());
+        factory(App\DatosPresupuesto::class)->create()->each(function ($u){
+            $u->presupuesto->precios()->save(factory(App\Precio::class)->make());
         });
     }
 }
